8c97513c1f0614885b01629e1eee26e5ca73f171
Jerome Martinez <jerome.martinez@livestrong.org>
2015-08-10 14:50:29 -0500

    changes put in for what-we-do env

--- app.py
+++ app.py
@@ -4,16 +4,9 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return render_template('base.html')
-
-@app.route("/practice")
-def practice():
-    return render_template('practice.html')
-
-@app.route("/tiny")
-def tiny():
-    return render_template('tiny.html')
+def what_we_do():
+    # return render_template('index.html')
+    return render_template('what-we-do.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- readme.md
+++ readme.md
@@ -2,6 +2,5 @@ Prototype for the new homepage for Livestrong.org
 
 - Make sure you have [Flask](http://flask.pocoo.org/) installed.
 - in one terminal: run `python app.py` in the root directory
-- in a separate terminal: run `stylus -w -o ../` in the static/design/styl/ directory
+- in a separate terminal: run `stylus -w -o ../static/css/` in the static/design/styl/ directory
 - open [http://localhost:5000/](localhost:5000/)
-
